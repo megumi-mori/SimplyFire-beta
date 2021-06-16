f90dcb79c17c3a3dce2f9dcbaf152a25c35c0c66
Megumi <megumi.mori@hotmail.com>
2021-06-16 00:24:57 -0700

    linked graph_panel scroll buttons with plot area (blank axes)

--- pymini/data_panel/graph_panel.py
+++ pymini/data_panel/graph_panel.py
@@ -2,10 +2,11 @@ import tkinter as Tk
 from tkinter import ttk
 from utils import widget
 from utils.scrollable_option_frame import ScrollableOptionFrame
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from config import config
+
 
 from data_panel.plot_area import InteractivePlot
+import pymini
 
 def load(parent):
     frame = ScrollableOptionFrame(parent, False)
@@ -47,8 +48,15 @@ def load(parent):
     yscrollbar_frame.grid_rowconfigure(1, weight=1)
 
 
-    ttk.Button(yscrollbar_frame, text='^').grid(column=0, row=0, sticky='news')
-    ttk.Button(yscrollbar_frame, text='v').grid(column=0, row=2, sticky='news')
+    pan_up = ttk.Button(yscrollbar_frame, text='^')
+    pan_up.grid(column=0, row=0, sticky='news')
+    pan_up.bind('<ButtonPress-1>', lambda e, c='y', d=1: scroll_plot(c, d))
+    pan_up.bind('<ButtonRelease-1>', stop)
+    pan_down = ttk.Button(yscrollbar_frame, text='v')
+    pan_down.grid(column=0, row=2, sticky='news')
+    pan_down.bind('<ButtonPress-1>', lambda e, c='y', d=-1: scroll_plot(c, d))
+    pan_down.bind('<ButtonRelease-1>', stop)
+
 
     frame.widgets['y_scrollbar'] = widget.LinkedScale(parent=yscrollbar_frame,
                                                 name='y_scrollbar',
@@ -57,6 +65,8 @@ def load(parent):
                                                 orient=Tk.VERTICAL,
                                                 command=None)
     frame.widgets['y_scrollbar'].widget.grid(column=0, row=1, sticky='news')
+    frame.widgets['y_scrollbar'].widget.config(state='disabled')
+    frame.widgets['y_scrollbar'].set(50)
 
     graph_frame = Tk.Frame(big_frame)
     graph_frame.grid(column=1, row=1, sticky='news')
@@ -64,6 +74,7 @@ def load(parent):
     graph_frame.grid_rowconfigure(0, weight=1)
 
     plot = InteractivePlot(graph_frame)
+    frame.plot = plot
     plot.frame.grid(column=0, row=0, sticky='news')
 
     toolbar_frame = Tk.Frame(big_frame)
@@ -74,23 +85,30 @@ def load(parent):
     navigation_toolbar.grid(column=0, row=0, sticky='news')
     navigation_toolbar.update()
 
-    channel_option = widget.LabeledOptionMenu(toolbar_frame,
+    frame.widgets['channel_option'] = widget.LabeledOptionMenu(toolbar_frame,
                                               label='channel',
                                               value='0',
                                               default='0',
                                               options=[0])
-    channel_option.frame.grid(column=2, row=0, sticky='news')
-    #
+    frame.widgets['channel_option'].frame.grid(column=2, row=0, sticky='news')
+
     x_zoom_frame = Tk.Frame(frame, bg='orange')
     x_zoom_frame.grid_rowconfigure(0, weight=1)
     x_zoom_frame.grid_columnconfigure(3, weight=1)
     x_zoom_frame.grid(column=0, row=2, sticky='news')
-    #
+
     ttk.Button(x_zoom_frame, text='+').grid(column=0, row=0, sticky='news')
     ttk.Button(x_zoom_frame, text='-').grid(column=1, row=0, sticky='news')
 
-    ttk.Button(x_zoom_frame, text='<').grid(column=2, row=0, sticky='news')
-    ttk.Button(x_zoom_frame, text='>').grid(column=4, row=0, sticky='news')
+    pan_left = ttk.Button(x_zoom_frame, text='<')
+    pan_left.grid(column=2, row=0, sticky='news')
+    pan_left.bind('<ButtonPress-1>', lambda e, c='x', d=-1: scroll_plot(c, d))
+    pan_left.bind('<ButtonRelease-1>', stop)
+
+    pan_right = ttk.Button(x_zoom_frame, text='>')
+    pan_right.grid(column=4, row=0, sticky='news')
+    pan_right.bind('<ButtonPress-1>', lambda e, c='x', d=1: scroll_plot(c, d))
+    pan_right.bind('<ButtonRelease-1>', stop)
 
 
     frame.widgets['x_scrollbar'] = widget.LinkedScale(parent=x_zoom_frame,
@@ -100,5 +118,20 @@ def load(parent):
                                                 orient=Tk.HORIZONTAL,
                                                 command=None)
     frame.widgets['x_scrollbar'].widget.grid(column=3, row=0, sticky='news')
+    frame.widgets['x_scrollbar'].widget.config(state='disabled')
+    frame.widgets['x_scrollbar'].set(50)
 
     return frame
+
+def scroll_plot(axis, dir):
+    scroll_plot_repeat(axis, dir)
+    return None
+
+def scroll_plot_repeat(axis, dir):
+    global jobid
+    jobid = pymini.root.after(config.scroll_wait, scroll_plot_repeat, axis, dir)
+    pymini.gp.plot.scroll(axis, dir)
+    return None
+
+def stop(e=None):
+    pymini.root.after_cancel(jobid)
--- pymini/data_panel/plot_area.py
+++ pymini/data_panel/plot_area.py
@@ -1,4 +1,5 @@
 import tkinter as Tk
+from config import config
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -16,3 +17,26 @@ class InteractivePlot():
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame)
         self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
         self.ax.plot()
+
+
+    def scroll(self, axis, dir = 1):
+        if axis == "x":
+            win_lim = self.ax.get_xlim()
+        elif axis == "y":
+            win_lim = self.ax.get_ylim()
+
+        else:
+            return None
+        width = win_lim[1] - win_lim[0]
+        delta = width * config.scroll_amount / 100
+        new_lim = (win_lim[0] + delta * dir, win_lim[1] + delta * dir)
+
+        if axis == "x":
+            self.ax.set_xlim(new_lim)
+        else:
+            self.ax.set_ylim(new_lim)
+        self.canvas.draw()
+
+    def scroll_y(self):
+        window_ylim = self.ax.get_ylim()
+        window_height = window_ylim[1] - window_ylim[0]
