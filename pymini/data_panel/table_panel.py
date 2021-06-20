feec316f9f150ff5eba67b019f08d66773a80d36
Megumi <megumi.mori@hotmail.com>
2021-06-20 12:59:36 -0700

    tried making the treeview an object rather than the frame

--- pymini/data_panel/table_panel.py
+++ pymini/data_panel/table_panel.py
@@ -2,82 +2,131 @@ import tkinter as Tk
 from tkinter import ttk
 import pandas as pd
 import pymini
+from data_panel.event_dataframe import EventDataFrame
+from collections import OrderedDict #Python 3.7+ can use dict
+
+
+
+
 def load(parent):
-    return InteractiveTable(parent)
-class InteractiveTable(Tk.Frame):
-    def __init__(self, parent):
-        Tk.Frame.__init__(self, parent, bg='gray')
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+    frame = Tk.Frame(parent)
+    frame.grid_rowconfigure(0, weight=1)
+    frame.grid_columnconfigure(0, weight=1)
 
-        self.table = ttk.Treeview(self, selectmode='extended')
-        self.table.grid(column=0, row=1, sticky='news')
+    table = InteractiveTable(frame)
+    table.grid(column=0, row=0, sticky='news')
 
-        vsb = ttk.Scrollbar(self, orient=Tk.VERTICAL, command=self.table.yview)
-        vsb.grid(column=1, row=1, sticky='ns')
-        self.table.configure(yscrollcommand=vsb.set)
+    vsb = ttk.Scrollbar(frame, orient=Tk.VERTICAL, command=table.yview)
+    vsb.grid(column=1, row=0, sticky='ns')
+    table.configure(yscrollcommand=vsb.set)
 
-        hsb = ttk.Scrollbar(self, orient=Tk.HORIZONTAL, command=self.table.xview)
-        hsb.grid(column=0, row=2, sticky='ew')
-        self.table.configure(xscrollcommand=hsb.set)
+    frame.table = table
 
-        # add any other columns needed here to translate from name of widget to header info
-        self.header = {
-            'data_display_time': 't',
-            'data_display_amplitude': 'Amp',
-            'data_display_amp_unit': 'unit',
-            'data_display_decay' : 'Decay (ms)',
-            'data_display_decay_time': 't (decay)',
-            'data_display_rise': 'Rise (ms)',
-            'data_display_start': 't (start)',
-            'data_display_end': 't (end)',
-            'data_display_channel': 'Ch'
-        }
+    hsb = ttk.Scrollbar(frame, orient=Tk.HORIZONTAL, command=table.xview)
+    hsb.grid(column=0, row=1, sticky='ew')
+    table.configure(xscrollcommand=hsb.set)
 
-        self.table.config(columns=[k for k in self.header.keys()], show='headings')
+    config2header = OrderedDict([
+        ('data_display_time', 't'),
+        ('data_display_amplitude','amp'),
+        ('data_display_amp_unit', 'amp_unit'),
+        ('data_display_decay_constant', 'decay_const'),
+        ('data_display_decay_unit', 'decay_unit'),
+        ('data_display_decay_time', 'decay_t'),
+        ('data_display_rise_constant', 'rise_const'),
+        ('data_display_rise_unit', 'rise_unit'),
+        ('data_display_baseline', 'baseline'),
+        ('data_display_baseline_unit', 'baseline_unit'),
+        ('data_display_start', 't_start'),
+        ('data_display_end', 't_end'),
+        ('data_display_channel', 'channel')
+    ])
 
-        # first show all data
-        for i, col in enumerate(self.header):
-            self.table.heading(i, text=self.header[col], command=lambda _col=col: self._sort(_col, False))
-            self.table.column(i, width=80, stretch=Tk.NO)
-        # self.fit_columns()
+    table.config(columns=[k for k in config2header.keys()], show='headings')
+    for i, col in enumerate(config2header):
+        table.heading(i, text=config2header[col], command=lambda _col=col: table._sort(_col, False))
+        table.column(i, width=80, stretch=Tk.NO)
 
-        # need to bind
+    return frame
+class InteractiveTable(ttk.Treeview):
+    def __init__(
+            self,
+            parent,
+            **kwargs
+    ):
+        super().__init__(parent, **kwargs)
+        self.data = EventDataFrame(
+            # need data for table panel AND for event markers!
+            columns=[
+                # panel -- make sure this matches with the config2header dict
+                't',
+                'amp',
+                'amp_unit',
+                'decay_const',
+                'decay_unit',
+                'decay_t',
+                'rise_const',
+                'rise_unit',
+                'baseline',
+                'baseline_unit',
+                't_start',
+                't_end',
+                'channel',
+                # plot
+                'peak_coord',  # (x,y)
+                'decay_coord',
+                'start_coord',
+                'end_coord'
+            ]
+        )
+
+    ##################################################
+    #                      Data                      #
+    ##################################################
+    def add_event(self, data):
+        """
+        adds data to the dataframe and the table
+        :param data: dict of data obtained from analysis
+        :return:
+        """
+        self.data.append(data)
+        # self.table.
+
+
+
+    ##################################################
+    #                Sortable Columns                #
+    ##################################################
+
+    def _sort(table, col, reverse):
+    # sort based on tutorial from Tek Recipes
+    # https://tekrecipes.com/2019/04/20/tkinter-treeview-enable-sorting-upon-clicking-column-headings/
+        try:
+            selected_iids = self.selection()  # may need to debug this later - keeping a finger on all selected values
+        except:
+            pass
+        try:
+            l = [(float(table.set(k, col)), k) for k in table.get_children('')]
+        except:
+            l = [(table.set(k, col), k) for k in table.get_children('')]
+        l.sort(reverse=reverse)
+        for index, (val, k) in enumerate(l):
+            table.move(k, '', index)
+        table.heading(col, command=lambda _col=col: table._sort(_col, not reverse))
 
     def show_columns(self):
         cols = pymini.tabs['detector_tab'].get_value_dict(filter='data_display_')
-        self.table.config(displaycolumns=tuple([i for i, e in enumerate(cols) if cols[e]]))
+        self.config(displaycolumns=tuple([i for i, e in enumerate(cols) if cols[e]]))
 
     def fit_columns(self):
         cols = pymini.tabs['detector_tab'].get_value_dict(filter='data_display_')
         indices = [i for i, e in enumerate(cols) if cols[e]]
-        w = int(self.table.winfo_width() / len(indices))
+        w = int(self.winfo_width() / len(indices))
         for i in indices:
-            self.table.column(i, width=w)
-
-
-    def _sort(self, col, reverse):
-        # sort based on tutorial from Tek Recipes
-        # https://tekrecipes.com/2019/04/20/tkinter-treeview-enable-sorting-upon-clicking-column-headings/
-        try:
-            selected_iids = self.table.selection() #may need to debug this later - keeping a finger on all selected values
-        except:
-            pass
-        try:
-            l = [(float(self.table.set(k, col)), k) for k in self.table.get_children('')]
-        except:
-            l = [(self.table.set(k, col), k) for k in self.table.get_children('')]
-        l.sort(reverse=reverse)
-        for index, (val, k) in enumerate(l):
-            self.table.move(k, '', index)
-        self.table.heading(col, command=lambda _col=col:self._sort(_col, not reverse))
+            self.column(i, width=w)
 
 
 
-
-
-    def display_column(self):
-        pass
 
 
 
