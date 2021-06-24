2607c785a164ddabadf1b5d2385d052bfc4575ab
Megumi <megumi.mori@hotmail.com>
2021-06-24 10:54:02 -0700

    sorting and cleaning

--- pymini/DataVisualizer/data_display.py
+++ pymini/DataVisualizer/data_display.py
@@ -2,56 +2,66 @@ import tkinter as Tk
 from tkinter import ttk
 from DataVisualizer.table import InteractiveTable
 # from data_panel.event_dataframe import EventDataFrame
-from collections import OrderedDict #Python 3.7+ can use dict
+from collections import OrderedDict  # Python 3.7+ can use dict
 import pymini
+
+# just use this to add something:
+# try:
+#     self.insert("", 'end',
+#                 values=[data[i] for i in self.columns],
+#                 iid=data[self.id])  # should have error if already exists
+# use this to remove something(s)
+# def delete(self, *items):
+#     super().delete(*items)
+
 header2config = OrderedDict([
-        ('t', 'data_display_time'),
-        ('amp', 'data_display_amplitude'),
-        ('amp_unit', 'data_display_amplitude'),
-        ('decay_const', 'data_display_decay'),
-        ('decay_unit', 'data_display_decay'),
-        ('decay_t', 'data_display_decay_time'),
-        ('rise_const', 'data_display_rise'),
-        ('rise_unit', 'data_display_rise'),
-        ('halfwidth', 'data_display_halfwidth'),
-        ('halfwidth_unit', 'data_display_halfwidth'),
-        ('baseline', 'data_display_baseline'),
-        ('baseline_unit', 'data_display_baseline'),
-        ('t_start', 'data_display_start'),
-        ('t_end', 'data_display_end'),
-        ('channel', 'data_display_channel', )
-    ])
+    ('t', 'data_display_time'),
+    ('amp', 'data_display_amplitude'),
+    ('amp_unit', 'data_display_amplitude'),
+    ('decay_const', 'data_display_decay'),
+    ('decay_unit', 'data_display_decay'),
+    ('decay_t', 'data_display_decay_time'),
+    ('rise_const', 'data_display_rise'),
+    ('rise_unit', 'data_display_rise'),
+    ('halfwidth', 'data_display_halfwidth'),
+    ('halfwidth_unit', 'data_display_halfwidth'),
+    ('baseline', 'data_display_baseline'),
+    ('baseline_unit', 'data_display_baseline'),
+    ('t_start', 'data_display_start'),
+    ('t_end', 'data_display_end'),
+    ('channel', 'data_display_channel',)
+])
 columns = [
-        # panel -- make sure this matches with the config2header dict
-        't',  #
-        'amp',  #
-        'amp_unit',  #
-        'decay_const',
-        'decay_unit',
-        'decay_t',
-        'rise_const',  #
-        'rise_unit',  #
-        'halfwidth',
-        'halfwidth_unit',
-        'baseline',  #
-        'baseline_unit',  #
-        't_start',  #
-        't_end',  #
-        'channel',  #
-        # plot
-        'peak_coord_x',  # (x,y) #
-        'peak_coord_y',  #
-        'decay_coord_x',
-        'decay_coord_y',
-        'start_coord_x',  #
-        'start_coord_y',  #
-        'end_coord_x',  #
-        'end_coord_y'  #
-    ]
+    # panel -- make sure this matches with the config2header dict
+    't',  #
+    'amp',  #
+    'amp_unit',  #
+    'decay_const',
+    'decay_unit',
+    'decay_t',
+    'rise_const',  #
+    'rise_unit',  #
+    'halfwidth',
+    'halfwidth_unit',
+    'baseline',  #
+    'baseline_unit',  #
+    't_start',  #
+    't_end',  #
+    'channel',  #
+    # plot
+    'peak_coord_x',  # (x,y) #
+    'peak_coord_y',  #
+    'decay_coord_x',
+    'decay_coord_y',
+    'start_coord_x',  #
+    'start_coord_y',  #
+    'end_coord_x',  #
+    'end_coord_y'  #
+]
 config2header = OrderedDict([(header2config[key], key) for key in header2config.keys()])
 
 
-def load(parent, root):
+def load(parent):
     frame = Tk.Frame(parent)
     frame.grid_rowconfigure(0, weight=1)
     frame.grid_columnconfigure(0, weight=1)
@@ -60,12 +70,9 @@ def load(parent, root):
     table = ttk.Treeview(frame)
     table.grid(column=0, row=0, sticky='news')
 
-    #custom functions
-    table.set_id = lambda x: setattr(table, 'id', x)
     table.config(columns=columns, show='headings')
-
     for i, col in enumerate(columns):
-        table.heading(i, text=col)
+        table.heading(i, text=col, command=lambda _col=col: _sort(table, _col, False))
         table.column(i, width=80, stretch=Tk.NO)
 
     table.show_columns = show_columns
@@ -78,19 +85,18 @@ def load(parent, root):
     hsb = ttk.Scrollbar(frame, orient=Tk.HORIZONTAL, command=table.xview)
     hsb.grid(column=0, row=1, sticky='ew')
     table.configure(xscrollcommand=hsb.set)
-
-
-
-
-    table.set_id('t')
-
     return frame
 
+
+add = lambda x: table.insert("", 'end', iid=x.get('t', None), values=[x.get(i, None) for i in columns])
+
+
 def show_columns():
-    table.config(displaycolumns = tuple([
+    table.config(displaycolumns=tuple([
         i for i in header2config
         if pymini.get_value(header2config[i])
     ]))
+
 
 def fit_columns():
     indices = [i for i in header2config
@@ -99,6 +105,10 @@ def fit_columns():
     for i in indices:
         table.column(i, width=w)
 
+
+def clear():
+    table.selection_remove(*table.selection())
+    table.delete(*table.get_children())
 
 def _sort(tv, col, reverse):
     try:
@@ -109,13 +119,3 @@ def _sort(tv, col, reverse):
     for index, (val, k) in enumerate(l):
         tv.move(k, '', index)
     tv.heading(col, command=lambda _col=col: _sort(_col, not reverse))
-
-
-
-
-
-
-
-
-
-
