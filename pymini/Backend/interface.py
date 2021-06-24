59ef335d3f85e543e7bdbca978d46672d4f38c11
Megumi <megumi.mori@hotmail.com>
2021-06-24 10:53:38 -0700

    got back the functionality to clear data when opening a new file (via interface)

--- pymini/Backend/interface.py
+++ pymini/Backend/interface.py
@@ -1,12 +1,14 @@
 # takes input from the Data Visualizers and takes appropriate action
 import pymini
 from tkinter import filedialog
+from DataVisualizer import data_display
 
 def open_trace():
     f = filedialog.askopenfilename(title='Open', filetypes=[('abf files', "*.abf"), ('All files', '*.*')])
     if f:
         pymini.trace_filename = f
         print(f)
+    data_display.clear()
 
 def configure(key, value):
     globals()[key] = value
