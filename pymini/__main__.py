0d7de045ebfe730b02ddf72c7153d8681be69d83
Megumi <megumi.mori@hotmail.com>
2021-06-24 22:25:21 -0700

    test case in main

--- pymini/__main__.py
+++ pymini/__main__.py
@@ -9,6 +9,8 @@ import Backend
 if __name__ == '__main__':
 
     root = pymini.load()
+    ### testing purposes:
+    Backend.interface.open_trace('D:\\megum\\Documents\\GitHub\\PyMini\\test_recordings\\20112011-EJC test.abf')
     # root=pymini.root
     # pymini.plot_area.open_trace('D:\\megum\\Documents\\GitHub\\PyMini\\test_recordings\\20112011-EJC test.abf')
     root.mainloop()
