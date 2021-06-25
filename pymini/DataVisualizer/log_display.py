f35e82facf995a5d47f30c100b2ae27918069c22
Megumi <megumi.mori@hotmail.com>
2021-06-24 20:26:26 -0700

    inserting info = clear the entry panel, also, new function for system updates in log

--- pymini/DataVisualizer/log_display.py
+++ pymini/DataVisualizer/log_display.py
@@ -12,8 +12,10 @@ def load(parent):
         parent=frame,
         name='application_log',
         value='',
-        default=''
+        default='',
+        lock=True
     )
+
     pymini.widgets['application_log'] = log_text
 
     log_text.configure(state='disabled')
@@ -29,18 +31,18 @@ def load(parent):
     pymini.widgets['custom_log'] = widget.VarEntry(parent=insert_frame, name='custom_log', value='', default='')
     pymini.widgets['custom_log'].grid(column=1, row=0, sticky='news')
     pymini.widgets['custom_log'].configure(justify=Tk.LEFT)
-    pymini.widgets['custom_log'].bind('<Return>', update)
+    pymini.widgets['custom_log'].bind('<Return>', user_update)
 
     test_button = Tk.Button(insert_frame, text='test')
-    test_button.bind('<ButtonPress-1>', update)
+    test_button.bind('<ButtonPress-1>', user_update)
     test_button.grid(column=2, row=0, sticky='news')
 
 
     return frame
 
-def update(e=None):
-    print(pymini.widgets['custom_log'].get())
+def user_update(e=None):
     pymini.widgets['application_log'].insert(Tk.END, '@user: ')
     pymini.widgets['application_log'].insert(Tk.END, '{}\n'.format(pymini.widgets['custom_log'].get()))
+    pymini.widgets['custom_log'].set("")
 
-
+system_update = lambda msg: pymini.widgets['application_log'].insert(Tk.END, '@system:  {}\n'.format(msg))
