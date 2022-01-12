60ca460283c0ed7b8e688147b568b2404d540d04
megumi-mori <66839987+megumi-mori@users.noreply.github.com>
2022-01-11 21:40:09 -0800

    version 2.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='PyMini',
-    version='b0.1.6',
+    version='b2.0.2',
     author='Megumi Mori GitHub@megumi-mori',
     packages=['PyMini'],
     pakage_dir={'PyMini': 'PyMini'},
