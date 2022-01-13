c71f712f7c08b63f9ff9032d24fb53a16d9e191f
megumi-mori <megumi.mori@hotmail.com>
2022-01-12 17:01:41 -0800

    main window gets focus after splash screen

--- PyMini/__main__.py
+++ PyMini/__main__.py
@@ -1,13 +1,12 @@
-from PyMini import app
-
 import tkinter as Tk
 if __name__ == '__main__':
     splash = Tk.Tk()
     Tk.Label(splash, text='Now Loading').grid(column=0, row=0)
     splash.update()
+    from PyMini import app
     splash.after(0, splash.destroy)
-
     splash.mainloop()
 
     root = app.load()
+    root.focus_force()
     root.mainloop()
