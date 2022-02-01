e56fd90b7cb6c120704d1095647179f14afa2f88
megumi-mori <66839987+megumi-mori@users.noreply.github.com>
2022-02-01 00:33:20 -0800

    rename

--- PyMini/Modules/base_module.py
+++ PyMini/Modules/base_module.py
@@ -3,7 +3,7 @@ import yaml
 import importlib
 from tkinter import BooleanVar
 from PyMini import app
-class BaseParentModule():
+class BaseModule():
     def __init__(self,
                  name:str,
                  menu_label:str,
@@ -30,54 +30,79 @@ class BaseParentModule():
         self.name = name
         self.menu_label = menu_label
         self.tab_label = tab_label
-        self.menu_var = BooleanVar()
+        self.menu_var = BooleanVar(value=False)
         self.disable_stack = []
 
         self._load_components()
 
         app.menubar.window_menu.add_checkbutton(label=self.menu_label,
-                                                command=self.update_module_display,
+                                                command=self.toggle_module_display,
                                                 variable=self.menu_var,
                                                 onvalue=True,
                                                 offvalue=False)
 
-
-    def update_module_display(self, event=None):
-        if self.menu_var.get(): # menu checkbutton is ON
+    def toggle_module_display(self, event=None):
+        if self.menu_var.get():
             self.show_tab()
             if not self.disable_stack:
                 self.select()
         else:
-            self.hide_tab()
+            self.hide()
+
+    def update_module_display(self, event=None):
+        if self.menu_var.get(): # menu checkbutton is ON
+            self.show_tab()
+        else:
+            self.hide()
 
     def show_tab(self, event=None):
+        try:
+            current_tab = app.cp_notebook.index(app.cp_notebook.select())
+        except:
+            current_tab = None
+        print(f'showing tab for {self.name}: {self.disable_stack}')
         if not self.disable_stack:
             if self.control_tab:
                 app.config_cp_tab(self.control_tab, state='normal')
             if self.data_tab:
                 app.config_data_tab(self.data_tab, state='normal')
+                print(self.name)
+                self.data_tab.fit_columns()
         else:
             if self.control_tab:
                 app.config_cp_tab(self.control_tab, state='disabled')
             if self.data_tab:
                 app.config_data_tab(self.data_tab, state='disabled')
+        try:
+            app.cp_notebook.select(current_tab)
+        except:
+            pass
 
-    def disable_tab(self, source:str):
+    def _disable(self, event=None, source:str=None):
+        source = self.name
         if source not in self.disable_stack:
             self.disable_stack.append(source)
+        print(f'_disable was called for {self.name}: {self.disable_stack}')
         self.update_module_display()
 
-    def force_enable_tab(self):
+    def force_enable(self):
         self.disable_stack = []
         self.update_module_display()
 
-    def enable_tab(self, source:str):
+    def _enable(self, event=None, source:str=None):
+        source = self.name
         try:
             self.disable_stack.remove(source)
         except ValueError:
             self._error_log(f'{source} is not part of the disable stack')
+        print(f'_enable was called for {self.name}: {self.disable_stack}')
         self.update_module_display()
 
+    def disable_module(self, modulename):
+        app.modules_dict[modulename]._disable(source=self.name)
+
+    def enable_module(self, modulename):
+        app.modules_dict[modulename]._enable(source=self.name)
     def is_visible(self):
         return self.menu_var.get()
 
@@ -85,11 +110,17 @@ class BaseParentModule():
         return self.is_visible() and not self.disable_stack
 
 
-    def hide_tab(self, event=None):
+    def hide(self, event=None):
         if self.control_tab:
             app.config_cp_tab(self.control_tab, state='hidden')
         if self.data_tab:
             app.config_data_tab(self.data_tab, state='hidden')
+
+    def select(self, event=None):
+        if self.control_tab:
+            app.cp_notebook.select(self.control_tab)
+        if self.data_tab:
+            app.data_notebook.select(self.data_tab)
 
     def _error_log(self, msg):
         # connect to log later
@@ -101,7 +132,7 @@ class BaseParentModule():
             return
         # make empty components:
         self.control_tab = None
-        self.table_tab = None
+        self.data_tab = None
         for component, details in component_dict.items(): # 'py file name': {dict of details}
             if details['location'] == 'control_panel':
                 module_py = importlib.import_module(f'PyMini.Modules.{self.name}.{component}')
@@ -110,7 +141,7 @@ class BaseParentModule():
                 # except TypeError:
                 #     self._error_log(f'component {component} does not have attribute ModuleControl')
             elif details['location'] == 'data_notebook':
-                module_py = importlib.import_module(f'{self.name}.{component}', package='Modules')
+                module_py = importlib.import_module(f'PyMini.Modules.{self.name}.{component}')
                 try:
                     self.data_tab = getattr(module_py, 'ModuleTable', None)(self)
                 except TypeError:
@@ -118,3 +149,6 @@ class BaseParentModule():
                     pass
 
 
+    def insert_file_menu(self):
+        self.file_menu = Tk.Menu(app.menubar.file_menu, tearoff=0)
+        app.menubar.file_menu.add_cascade(label=self.name, menu=self.file_menu)
