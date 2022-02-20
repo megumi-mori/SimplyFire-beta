7fdb5a3605ebddcd0b1b8c2c359663c4d9677d29
megumi-mori <66839987+megumi-mori@users.noreply.github.com>
2022-02-20 12:36:46 -0800

    copy from template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,11 +2,12 @@
 
 # -- Project information
 
-project = 'SimplyFire'
-copyright = '2022, Megumi Mori'
-author = 'Megumi Mori'
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
 
-version = '0.3.0'
+release = '0.1'
+version = '0.1.0'
 
 # -- General configuration
 
@@ -24,11 +25,11 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
-# templates_path = ['_templates']
-#
-# # -- Options for HTML output
-#
-# html_theme = 'sphinx_rtd_theme'
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
