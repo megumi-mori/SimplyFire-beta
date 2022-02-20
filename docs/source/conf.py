deab768088fda1918a1e723b1e878138295b5c2d
megumi-mori <66839987+megumi-mori@users.noreply.github.com>
2022-02-20 13:38:58 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,12 +33,12 @@ pygments_style = 'sphinx'
 # import sphinx_rtd_theme
 html_theme = 'default'
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+#
+# if not on_rtd:  # only import and set the theme if we're building docs locally
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
 
